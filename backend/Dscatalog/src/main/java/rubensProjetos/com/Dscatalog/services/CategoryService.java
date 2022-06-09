c9fc4b8b7bfc2f2e98c9e0d739b0dce8574ea644
rubens.junior@outlook.com.br <rubens.junior@outlook.com.br>
2022-06-09 00:31:30 -0300

    Delete categoria

--- backend/Dscatalog/src/main/java/rubensProjetos/com/Dscatalog/services/CategoryService.java
+++ backend/Dscatalog/src/main/java/rubensProjetos/com/Dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import rubensProjetos.com.Dscatalog.dto.CategoryDTO;
 import rubensProjetos.com.Dscatalog.entities.Category;
 import rubensProjetos.com.Dscatalog.repositories.CategoryRepository;
+import rubensProjetos.com.Dscatalog.services.exceptions.DataBaseException;
 import rubensProjetos.com.Dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -49,9 +52,21 @@ public class CategoryService {
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
-		} 
-		catch (EntityNotFoundException e) {
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id não encontrado" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Violação de Integridade");
 		}
 	}
 }
---
+++ backend/Dscatalog/src/main/java/rubensProjetos/com/Dscatalog/services/exceptions/DataBaseException.java
@@ -0,0 +1,11 @@
+package rubensProjetos.com.Dscatalog.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException (String msg) {
+		super (msg);
+	}
+
+}
